2e268aa2aca8507136d6fe675cd6bb3fdd0fdc7d
nicu <nicu@spotify.com>
2016-06-21 15:35:54 +0200

    Converted to ES5

--- app.js
+++ app.js
@@ -1,23 +1,23 @@
-const SAMPLE_TIME = 0.1;
+var SAMPLE_TIME = 0.1;
 
-const video = document.getElementById('video');
-const canvas = document.getElementById('canvas');
-const context = canvas.getContext('2d');
-const fyuse = document.getElementById('fyuse');
+var video = document.getElementById('video');
+var canvas = document.getElementById('canvas');
+var context = canvas.getContext('2d');
+var fyuse = document.getElementById('fyuse');
 
-let initFyuse = () => {
+var initFyuse = function() {
   document.querySelector('.loading').remove();
   document.querySelector('canvas').remove();
 
-  let frames = document.querySelectorAll('#fyuse img');
-  let frameCount = frames.length;
-  let frameWidth = video.videoWidth / frameCount;
+  var frames = document.querySelectorAll('#fyuse img');
+  var frameCount = frames.length;
+  var frameWidth = video.videoWidth / frameCount;
 
   document.querySelector('#fyuse img').classList.add('visible');
 
   fyuse.addEventListener('mousemove', (evt) => {
-    let prev = document.querySelector('#fyuse .visible');
-    let frameIndex = Math.round(evt.clientX / frameWidth);
+    var prev = document.querySelector('#fyuse .visible');
+    var frameIndex = Math.round(evt.clientX / frameWidth);
 
     if ((frames[frameIndex] !== prev) && frames[frameIndex]) {
       prev && prev.classList.remove('visible');
@@ -26,17 +26,17 @@ let initFyuse = () => {
   }, false);
 };
 
-let captureFrame = () => {
+var captureFrame = function() {
   context.drawImage(video, 0, 0, video.videoWidth, video.videoHeight);
-  let dataUrl = canvas.toDataURL();
+  var dataUrl = canvas.toDataURL();
 
-  let img = document.createElement('img');
+  var img = document.createElement('img');
   img.setAttribute('src', dataUrl);
 
   fyuse.appendChild(img);
-}
+};
 
-video.addEventListener('loadeddata', () => {
+video.addEventListener('loadeddata', function() {
   canvas.width = video.videoWidth;
   canvas.height = video.videoHeight;
   canvas.style.width = video.videoWidth;
@@ -47,10 +47,10 @@ video.addEventListener('loadeddata', () => {
   video.currentTime += SAMPLE_TIME;
 }, false);
 
-video.addEventListener('seeked', () => {
+video.addEventListener('seeked', function() {
   captureFrame();
 
-  let currentTime = video.currentTime;
+  var currentTime = video.currentTime;
   currentTime += SAMPLE_TIME;
 
   if (currentTime <= video.duration) {
