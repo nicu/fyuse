e45d58ced4f22f1fd8188087f7ecfcbe6ff35a81
nicu <nicu@spotify.com>
2016-06-21 16:23:45 +0200

    Test

--- app.js
+++ app.js
@@ -1,17 +1,23 @@
-var SAMPLE_TIME = 0.1;
-
-var video = document.getElementById('video');
-var canvas = document.getElementById('canvas');
-var context = canvas.getContext('2d');
 var fyuse = document.getElementById('fyuse');
+var frames = [];
+var WIDTH = 272;
+
+function loadFrame(index) {
+  return new Promise(function(resolve, reject) {
+    var image = new Image();
+    image.src = 'frames/Frame' + (index+1) + '.png';
+    image.onload = resolve;
+    frames[index] = image;
+
+    fyuse.appendChild(image);
+  });
+}
 
 var initFyuse = function() {
   document.querySelector('.loading').remove();
-  document.querySelector('canvas').remove();
 
-  var frames = document.querySelectorAll('#fyuse img');
   var frameCount = frames.length;
-  var frameWidth = video.videoWidth / frameCount;
+  var frameWidth = WIDTH / frameCount;
 
   document.querySelector('#fyuse img').classList.add('visible');
 
@@ -26,37 +32,9 @@ var initFyuse = function() {
   }, false);
 };
 
-var captureFrame = function() {
-  context.drawImage(video, 0, 0, video.videoWidth, video.videoHeight);
-  var dataUrl = canvas.toDataURL();
+var promises = [];
+for (var i = 0; i < 51; i++) {
+  promises.push(loadFrame(i));
+}
 
-  var img = document.createElement('img');
-  img.setAttribute('src', dataUrl);
-
-  fyuse.appendChild(img);
-};
-
-video.addEventListener('loadeddata', function() {
-  canvas.width = video.videoWidth;
-  canvas.height = video.videoHeight;
-  canvas.style.width = video.videoWidth;
-  canvas.style.height = video.videoHeight;
-
-  captureFrame();
-
-  video.currentTime += SAMPLE_TIME;
-}, false);
-
-video.addEventListener('seeked', function() {
-  captureFrame();
-
-  var currentTime = video.currentTime;
-  currentTime += SAMPLE_TIME;
-
-  if (currentTime <= video.duration) {
-    video.currentTime = currentTime;
-  }
-  else {
-    initFyuse();
-  }
-});
+Promise.all(promises).then(initFyuse);
