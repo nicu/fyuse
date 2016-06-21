56b6cc2fb2ea60b17b40379a5ad5a0aa544977ef
nicu <nicu@spotify.com>
2016-06-21 15:31:02 +0200

    Checkpoint

--- app.js
+++ app.js
@@ -7,17 +7,18 @@ const fyuse = document.getElementById('fyuse');
 
 let initFyuse = () => {
   document.querySelector('.loading').remove();
-  
+  document.querySelector('canvas').remove();
+
   let frames = document.querySelectorAll('#fyuse img');
   let frameCount = frames.length;
   let frameWidth = video.videoWidth / frameCount;
-  
+
   document.querySelector('#fyuse img').classList.add('visible');
-  
+
   fyuse.addEventListener('mousemove', (evt) => {
     let prev = document.querySelector('#fyuse .visible');
     let frameIndex = Math.round(evt.clientX / frameWidth);
-    
+
     if ((frames[frameIndex] !== prev) && frames[frameIndex]) {
       prev && prev.classList.remove('visible');
       frames[frameIndex] && frames[frameIndex].classList.add('visible');
@@ -28,10 +29,10 @@ let initFyuse = () => {
 let captureFrame = () => {
   context.drawImage(video, 0, 0, video.videoWidth, video.videoHeight);
   let dataUrl = canvas.toDataURL();
-  
+
   let img = document.createElement('img');
   img.setAttribute('src', dataUrl);
-  
+
   fyuse.appendChild(img);
 }
 
@@ -40,18 +41,18 @@ video.addEventListener('loadeddata', () => {
   canvas.height = video.videoHeight;
   canvas.style.width = video.videoWidth;
   canvas.style.height = video.videoHeight;
-  
+
   captureFrame();
-  
+
   video.currentTime += SAMPLE_TIME;
 }, false);
 
 video.addEventListener('seeked', () => {
   captureFrame();
-  
+
   let currentTime = video.currentTime;
   currentTime += SAMPLE_TIME;
-  
+
   if (currentTime <= video.duration) {
     video.currentTime = currentTime;
   }
